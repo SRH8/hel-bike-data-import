de52fb4de4c3dfbb3c3122eee5e6efe585663f03
Sergio <Sergio Fraga>
2023-04-27 13:03:38 +0200

    First commit

---
+++ src/main/java/com/sergiofraga/helbikedataimport/HelBikeDataImportApplication.java
@@ -0,0 +1,13 @@
+package com.sergiofraga.helbikedataimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelBikeDataImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelBikeDataImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiofraga/helbikedataimport/HelBikeDataImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiofraga.helbikedataimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelBikeDataImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
